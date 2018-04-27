c31e430f287025542f50554c8432b2055ea1accc
Fangnan <fangnan@upenn.edu>
2018-04-27 15:14:35 -0400

    change

--- examples/torque/js/setup.js
+++ examples/torque/js/setup.js
@@ -1,7 +1,7 @@
 // Leaflet map setup
 var map = L.map('map', {
-  center: [40.75583970971843, -73.795166015625],
-  zoom: 11
+  center: [-33.438321, -70.690991],
+  zoom: 4
 });
 
 var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
@@ -12,59 +12,66 @@ var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{
   ext: 'png'
 }).addTo(map);
 
-var testFunction = function() {
+/*var testFunction = function() {
   var test = 123
   console.log("testing");
   test = 234
   return test
 };
+*/
 
 // Include this CSS so that torque knows what to do
 var CARTOCSS = [
   'Map {',
-  '-torque-frame-count:256;',
-  '-torque-animation-duration:30;',
-  '-torque-time-attribute:"pickup_datetime";',
-  '-torque-aggregation-function:"count(cartodb_id)";',
-  '-torque-resolution:2;',
-  '-torque-data-aggregation:linear;',
+    '-torque-frame-count: 256;',
+    '-torque-animation-duration: 30;',
+    '-torque-time-attribute: "acq_date";',
+    '-torque-aggregation-function: "count(1)";',
+    '-torque-resolution: 4;',
+    '-torque-data-aggregation: linear;',
   '}',
-  '#ny_taxi_dec_2015_copy{',
-  'comp-op: lighter;',
-  'marker-fill-opacity: 0.9;',
-  'marker-line-color: #FFF;',
-  'marker-line-width: 0;',
-  'marker-line-opacity: 1;',
-  'marker-type: ellipse;',
-  'marker-width: 6;',
-  'marker-fill: #0F3B82;',
+  '#layer {',
+    'marker-width: 7;',
+    'marker-fill: #EE4D5A;',
+    'marker-fill-opacity: 0.9;',
+    'marker-line-width: 1;',
+    'marker-line-color: #FFFFFF;',
+    'marker-line-opacity: 1;',
+    'comp-op: lighter;',
   '}',
-  '#ny_taxi_dec_2015_copy[frame-offset=1] {',
-  'marker-width:8;',
-  'marker-fill-opacity:0.45; ',
+  '#layer[frame-offset=1] {',
+    'marker-width: 9;',
+    'marker-fill-opacity: 0.45;',
   '}',
-  '#ny_taxi_dec_2015_copy[frame-offset=2] {',
-  'marker-width:10;',
-  'marker-fill-opacity:0.225; ',
+  '#layer[frame-offset=2] {',
+    'marker-width: 11;',
+    'marker-fill-opacity: 0.225;',
   '}'
 ].join('\n');
 
 // Create the actual layer to be used
 var torqueLayer = new L.TorqueLayer({
-  user: 'npzimmerman',
+  user: 'fangnandu',
   cartocss: CARTOCSS
 });
 torqueLayer.addTo(map);
 
+torqueLayer.setSQL("SELECT * FROM fire_nrt_v1_5527");
+torqueLayer.play();
+
+/*
 // On timechange, update the HTML which hovers over the upper right of the map
 torqueLayer.on('change:time', function(d) {
   var time = $('<h3>').text('Time - ' + moment(d.time).format('HH:mm'));
   $('div#time-window div').empty();
   $('#time-window div').append(time);
 });
-
+*/
+//torqueLayer.setSQL("SELECT * FROM fire_nrt_v1_5527 ");
+//torqueLayer.play();
+/*
 // We'll just create some buttons for the first 7 days of cab data
-_.each([1, 2, 3, 4, 5, 6, 7], function(num) {
+_.each([1, 2, 3], function(num) {
   var button = $('<button>')
     .addClass('btn')
     .addClass('btn-default')
@@ -72,11 +79,12 @@ _.each([1, 2, 3, 4, 5, 6, 7], function(num) {
     .click(function() {
       $('#time-window').empty();
       $('#time-window')
-        .append($('<h1>').text('Date - 2015-12-0' + num))
+        .append($('<h1>').text('Date - 2015-'+ num))
         .append($('<div>'));
       torqueLayer.stop();
-      torqueLayer.setSQL("SELECT * FROM ny_taxi_dec_2015_copy WHERE (pickup_datetime >= ('2015-12-0" + num + "T00:00:00-05:00') AND pickup_datetime <= ('2015-12-0" + (num + 1) + "T00:00:00-05:00'))");
+      torqueLayer.setSQL("SELECT * FROM fire_nrt_v1_5527 WHERE (acq_date >= ('2018-0"+ num +"-01T00:00:00Z') AND pickup_datetime <= ('2018-0" + (num + 1) + "-01T00:00:00Z'))");
       torqueLayer.play();
     });
   $('#button-container').append(button);
 });
+*/
